b877f892d84419bd8fb211a40715d41346e319d7
alex <alexandros441@yahoo.gr>
2021-03-22 20:51:23 +0200

    refactored: __main__ into class

--- __main__.py
+++ __main__.py
@@ -6,39 +6,47 @@ import base64
 import errno
 import pickle
 
-NAME = "vault.vl"
+import shutil
 
-def encode_folder():
-    structure = []
-    for path in glob.glob(os.path.abspath('vault/**/*'), recursive=True):
-        relative_path = path.replace(os.getcwd(), '')
-        if(os.path.isfile(path)):
-            with open(path, 'rb') as fil:
-                structure.append([relative_path[1:], fil.read().decode('utf-8')])
-    structure_json = json.dumps(structure)
-    encode_structure = base64.b64encode(structure_json.encode("utf-8"))
+class SubVault:
+    def __init__(self, name="vault"):
+        self.NAME = name 
 
-    with open(NAME, 'wb') as fil:
-        pickle.dump(encode_structure, fil)
+    def encrypt(self):
+        if(not os.path.isdir(self.NAME)):
+            raise Exception(f'There is no folder with the name, {self.NAME}')
 
-def check_for_sub_directory(filename):
-    if not os.path.exists(os.path.dirname(filename)):
-        try:
-            os.makedirs(os.path.dirname(filename))
-        except OSError as exc:
-            if exc.errno != errno.EEXIST:
-                raise
+        structure = []
+        for path in glob.glob(os.path.abspath(f'{self.NAME}/**/*'), recursive=True):
+            relative_path = path.replace(os.getcwd(), '')
+            if(os.path.isfile(path)):
+                with open(path, 'rb') as fil:
+                    structure.append([relative_path[1:], fil.read().decode('utf-8')])
+        structure_json = json.dumps(structure)
+        encode_structure = base64.b64encode(structure_json.encode("utf-8"))
 
-def decode_folder():
-    with open(NAME, 'rb') as fil:
-        data = pickle.load(fil)
+        with open(f"{self.NAME}.vl", 'wb') as fil:
+            pickle.dump(encode_structure, fil)
 
-    decoded_structure = base64.b64decode(data)
-    structure = json.loads(decoded_structure)
-    for list_file in structure:
-        check_for_sub_directory(list_file[0])
+    def decrypt(self):
+        with open(f"{self.NAME}.vl", 'rb') as fil:
+            data = pickle.load(fil)
 
-        with open(list_file[0], 'wb') as fil:
-            fil.write(str.encode(list_file[1]))
+        decoded_structure = base64.b64decode(data)
+        structure = json.loads(decoded_structure)
 
-decode_folder()
+        for path, content in structure:
+            self.check_for_sub_directory(path)
+            with open(path, 'wb') as fil:
+                fil.write(str.encode(content))
+
+    def del_folder(self):
+        shutil.rmtree(self.NAME)
+
+    def check_for_sub_directory(self, filename):
+        if(not os.path.exists(os.path.dirname(filename))):
+            try:
+                os.makedirs(os.path.dirname(filename))
+            except OSError as exc:
+                if(exc.errno != errno.EEXIST):
+                    raise Exception('File already exists.')
